d7cbc9e8c8e537afc718a827abdd730c41d66f65
jossely25 <aguirre2.5jea@gmail.com>
2025-11-06 09:45:15 -0500

    Codigo adicional con txt

--- chat.py
+++ chat.py
@@ -1,9 +1,37 @@
+# Importar las librerías necesarias
 import pandas as pd
 from sentence_transformers import SentenceTransformer, util
 
-df = pd.read_excel("conocimiento.xlsx")
+#Codigo adicional para cargar archivos txt si es necesario
+def cargar_txt(ruta):
+    preguntas = []
+    respuestas = []
 
-modelo = SentenceTransformer('distilus-base-multilingual-cased-v2')
+#Codigo adicinal para abrir archivos txt base de conocimiento
+    with open(ruta, 'r', encoding='utf-8') as file:
+        contenido = file.read().split('---')
+
+# Codigo adicinal para recorrer los bloques y encuentre la respuesta correspondiente a cada pregunta
+        for bloque in contenido:
+            if 'PREGUNTA:' in bloque and 'RESPUESTA:' in bloque:
+
+# Codigo adicional para separar los bloque en lineas independientes
+                partes = bloque.strip().split('\n')
+
+# Codigo adicional para limpiar y ampliar los prefijos para una mejor intrepretacion de las preguntas y mejor calidad de respuestas
+                pregunta = partes[0].replace('PREGUNTA:','').strip()
+                respuesta = partes[1].replace('RESPUESTA:','').strip()
+                preguntas.append(pregunta)
+                respuestas.append(respuesta)
+
+# Codigo adicional para crear un DataFrame con pandas para mantener temporalmente las preguntas y respuestas como unas columnas
+    return pd.DataFrame({'Preguntas': preguntas, 'Respuestas': respuestas})
+
+# Fuente de datos pre-entrenados (se cambia la ruta y el tipo de archivo si es necesario)
+df = cargar_txt("Base de conocimiento.txt")
+#df = pd.read_excel("conocimiento.xlsx")
+
+modelo = SentenceTransformer('LaBSE')
 
 embedding_preguntas = modelo.encode(df['Preguntas'].tolist(), convert_to_tensor=True)
 
@@ -20,4 +48,3 @@ while True:
     idx = int(similitudes[0][0]['corpus_id'])
 
     print("\n Cariño: " + df['Respuestas'].iloc[idx])
-
